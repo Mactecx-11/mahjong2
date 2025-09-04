5361a62859667f71555f8b8fa4c9b517dd7e8cbc
Mactecx-11 <asjhand9@gmail.com>
2025-09-04 17:30:05 +0530

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // Fill these if you want remote internet play. For QR/manual (nearby), leave as-is.
 window.FIREBASE_CFG = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: "AIzaSyAyJqreTROYrVgxB1PySlW-vegBQ09B4lE",
+  authDomain: "mahjongs.firebaseapp.com",
   databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  projectId: "mahjongs",
+  storageBucket: "mahjongs.firebasestorage.app",
+  messagingSenderId: "671251231125",
+  appId: "1:671251231125:web:22a5284466b56dabf1069c"
 };
